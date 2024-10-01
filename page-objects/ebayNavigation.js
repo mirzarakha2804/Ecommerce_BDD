6ac17e554eb63030bcd456e562e4f7ef2179e213
mirzarakha28 <mirzarakhadwipradana28@gmail.com>
2024-10-01 13:13:11 +0700

    testcase ebay

--- page-objects/ebayNavigation.js
+++ page-objects/ebayNavigation.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 class eBayNavigation {
     async openHomepage() {
         await browser.url('https://www.ebay.com');
-        await browser.pause(2000); // Wait for page to load
+        await browser.pause(2000);
     }
 
     async navigateToCellPhones() {
@@ -11,8 +11,7 @@ class eBayNavigation {
         await browser.pause(2000);
         
         const ariaExpanded = await shopByCategoryButton.getAttribute('aria-expanded');
-        assert.strictEqual(ariaExpanded, 'true');  // After clicking, aria-expanded should be "true"
-        
+        assert.strictEqual(ariaExpanded, 'true');  
         const cellPhonesAndAccessoriesLink = await $('a.scnd=Cell phones & accessories');
         await cellPhonesAndAccessoriesLink.click();
         await browser.pause(2000);
@@ -25,9 +24,9 @@ class eBayNavigation {
 
     async clickCellPhonesAndSmartphones() {
         const cellPhonesAndSmartphonesLink = await $('a=Cell Phones & Smartphones');
-        await cellPhonesAndSmartphonesLink.waitForDisplayed(); // Wait for the link to be displayed
-        await cellPhonesAndSmartphonesLink.scrollIntoView(); // Scroll to the element if necessary
-        await cellPhonesAndSmartphonesLink.click(); // Click the link
+        await cellPhonesAndSmartphonesLink.waitForDisplayed();
+        await cellPhonesAndSmartphonesLink.scrollIntoView(); 
+        await cellPhonesAndSmartphonesLink.click(); 
         await browser.pause(2000);
 
         const titleSpan = await $('span=Cell Phones & Smartphones');
@@ -42,27 +41,19 @@ class eBayNavigation {
         await browser.pause(2000);
     }
     async searchProduct(product) {
-        const searchInput = await $('input[name="_nkw"]'); ; // Search input field
-        await searchInput.clearValue(); // Clear any existing text
-        await searchInput.setValue(product); // Enter the product name
+        const searchInput = await $('input[name="_nkw"]');
+        await searchInput.clearValue(); 
+        await searchInput.setValue(product);
         await browser.keys('Enter');
         await browser.pause(2000);
     }
 
     async changeCategory(category) {
         const dropdown = await $('select[aria-label="Select a category for search"]');
-        
         await dropdown.click();
-
         await browser.pause(2000);
         const categoryOption = await $(`option=${"└ "+category}`);
         await categoryOption.click();
-        // const searchInput = await $('input[name="_nkw"]'); ;
-        // searchInput.click();
-        // await browser.keys('Enter');
-        // await browser.pause(2000);
-
-        
     }
     async clickSearchButton() {
         const searchButton = await $('input[type="submit"]');
@@ -70,10 +61,10 @@ class eBayNavigation {
         await browser.pause(2000);
     } 
     async verifyPageLoad() {
-        const saveNullSearchDiv = await browser.$('.srp-save-null-search'); // Selector for the div
-        const exists = await saveNullSearchDiv.isExisting(); // Check if the element exists
+        const saveNullSearchDiv = await browser.$('.srp-save-null-search'); 
+        const exists = await saveNullSearchDiv.isExisting(); 
         if (exists) {
-            throw new Error('The srp-save-null-search div exists when it should not.'); // Throw an error if it exists
+            throw new Error('The srp-save-null-search div exists when it should not.'); 
         }
     }
 
